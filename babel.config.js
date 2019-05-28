8e8cc0bc625f8eaa343c166390ee5893ac2d4ca1
André Neves <aneves@decisiv.com>
2019-05-28 14:48:54 +0100

    fix(core): compile down to commonjs (#29)
    Fixes: #28

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,12 @@
 // https://www.wisdomgeek.com/development/web-development/how-to-setup-jest-typescript-babel-webpack-project/
 module.exports = (api) => {
-  // Testing if babel is being run in test mode
-  const isTest = api.env('test');
-
   /**
    * Cache the returned value forever and don't call this function again. This is the default behavior but since we
    * are reading the env value above, we need to explicitly set it after we are done doing that, else we get a
    * caching was left unconfigured error.
    */
   api.cache(true);
+
   return {
     presets: [
       [
@@ -17,12 +15,8 @@ module.exports = (api) => {
         {
           // Specifying which browser versions you want to transpile down to
           targets: '> 0.25%, not dead',
-          /**
-           * Specifying what module type should the output be in.
-           * For test cases, we transpile all the way down to commonjs since jest does not understand TypeScript.
-           * For all other cases, we don't transform since we want Webpack to do that in order for it to do
-           * dead code elimination (tree shaking) and intelligently select what all to add to the bundle.
-           */ modules: isTest ? 'commonjs' : false,
+
+          modules: 'commonjs',
         },
       ],
       // Enabling Babel to understand TypeScript
